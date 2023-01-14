9185ded0fe685ace2c1eb05ff30cf3e4a05baad5
nbenouerkhou <nasreddine.benouerkhou@gmail.com>
2023-01-14 22:04:02 +0100

    config

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+    
+    // constant couleur des titres //
+    const COLOR_TITRE ="colorMenu";
+    
+    //constant idstatut animal//
+    const ID_STATUT_A_L_ADOPTION = 1;
+    const ID_STATUT_ADOPTE = 2;
+    
+    //constant actualite//
+    const TYPE_NEWS = "news";
+    const TYPE_ACTIONS = "actions";
+    const TYPE_EVENTS = "events";
+
+   const COOKIE_PROTECT = "timer";
+
+   //constant type alert //
+
+   const ALERT_SUCCESS = 1;
+   const ALERT_DANGER = 2;
+   const ALERT_INFO = 3;
+   const ALERT_WARNING = 4;
+
+
+?>
